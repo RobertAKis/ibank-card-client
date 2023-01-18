b1d6f869f8343691f199f04a1da1a535e0063db4
RobertAKis <robert.kis@mtdtechnology.net>
2023-01-18 17:18:42 +0200

    first commit

---
+++ src/main/java/com/ibank/ibankcardclient/IbankCardClientApplication.java
@@ -0,0 +1,13 @@
+package com.ibank.ibankcardclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbankCardClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbankCardClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibank/ibankcardclient/IbankCardClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibank.ibankcardclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbankCardClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
